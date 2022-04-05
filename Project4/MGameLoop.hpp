3965ae2cb52d476d67e6b69d0126366d74880fbe
YUHAN <YUHAN@PC7207_06>
2022-04-05 15:43:14 +0900

    초기 커밋.

---
+++ Project4/MGameLoop.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <chrono>
+#include <thread>
+#include "MConsolUtil.hpp"
+#include <cmath>
+
+using namespace std;
+
+namespace MuSeoun_Engine
+{
+	class MGameLoop
+	{
+	private :
+		bool _isGameRunning;	
+		MConsoleRenderer cRenderer;
+		
+	public :
+		MGameLoop()
+		{ _isGameRunning = false; }
+		~MGameLoop() {}
+
+		void Run()
+		{
+			_isGameRunning = true;
+			Initialize();
+
+			while (_isGameRunning)
+			{
+				chrono::system_clock::time_point startRenderTimePoint = chrono::system_clock::now();
+				Input();
+				Update();
+				Render();
+				chrono::duration<double> renderDuration = chrono::system_clock::now() - startRenderTimePoint;
+				string fps = "FPS : " + to_string(1 / renderDuration.count());
+				cRenderer.DrawString(fps);
+			}
+			Release();
+		}
+		void Stop()
+		{ _isGameRunning = false; }
+
+	private :
+		void Initialize() {}
+		void Release() {}
+
+		void Input() {}
+		void Update() {}
+		void Render()
+		{
+			cRenderer.Clear();
+			cRenderer.MoveCursor(10, 20);
+		}
+	};
+
+	
+
+
+	
+
+}
