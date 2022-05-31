843674a484161c4ca501bca0054433fd9cda72e9
YUHAN <YUHAN@PC7207_14>
2022-05-31 13:52:36 +0900

    [GT] 왜 안되냐 엿같네

--- Project4/MGameLoop.hpp
+++ Project4/MGameLoop.hpp
@@ -135,10 +135,7 @@ namespace MuSeoun_Engine
 								count++;
 						}
 						if (count == 6)
-						{
 							t[arrNum]->Initialize();
-							printf("%f, %f\n", t[arrNum]->y, t[arrNum]->length);
-						}
 					}
 				}
 				for (size_t i = 0; i < trabSize; i++)
